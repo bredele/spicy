277a3d0196332fe117ef5d4244ce39d84b5228a8
bredele <olivier.wietrich@gmail.com>
2015-01-16 21:35:29 -0700

    curry function and variadic functions

---
+++ index.js
@@ -0,0 +1,41 @@
+
+/**
+ * Native function binding.
+ * Curry arguments by default.
+ * @type {Function}
+ */
+
+var bind = Function.prototype.bind;
+
+
+/**
+ * Curry function.
+ *
+ * Also provide an optional arity
+ * (useful for variadic functions).
+ *
+ * Examples:
+ *
+ *   var add = curry(function(a, b) {
+ *     return a + b;
+ *   });
+ *   add(1,2);
+ *   add(1)(2);
+ *
+ * @param {Function} fn
+ * @param {Number?} arity
+ * @return {Function}
+ * @api public 
+ */
+
+module.exports = function(fn, arity) {
+  var length = arity || fn.length;
+  return function() {
+    var args = [].slice.call(arguments);
+    var l = args.length;
+    if(l < length) {
+      return bind.apply(fn, [,].concat(args));
+    }
+    return fn.apply(null, args);
+  };
+};
