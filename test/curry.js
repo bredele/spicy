c009fcb699c7b2182e051a725538d78ac8cec177
bredele <olivier.wietrich@gmail.com>
2015-01-22 20:54:42 -0700

    partially evaluate a function

--- test/curry.js
+++ test/curry.js
@@ -14,5 +14,14 @@ describe("curry", function() {
 		});
 		assert.equal(add(1,2), 3);
 	});
+
+	it('should partially evaluate a function', function() {
+		var add = curry(function(a, b) {
+			return a + b;
+		});
+		var fn = add(1);
+		assert.equal(typeof fn, 'function');
+		assert.equal(fn(2), 3);
+	});
 	
 });
